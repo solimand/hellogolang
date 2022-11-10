9552df7d420aa6f3feb9f48bfd473306deb5d0ad
msolimando <michele.solimando@unibo.it>
2022-11-10 17:09:04 +0100

    Map Exercise - A tour of Go

---
+++ maps.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+// This function returns a map of the words in the string s and the number of occurrences of the words in the string
+func WordCount(s string) map[string]int {
+	var words = strings.Fields(s)
+	result := make(map[string]int)
+
+	// FIND DUPLICATES
+	for _, v := range words {
+		_, ok := result[v]
+		if ok {
+			result[v] += 1
+		} else {
+			result[v] = 1
+		}
+	}
+
+	fmt.Println(result)
+	//return map[string]int{"x": 1}
+	return result
+}
+
+func main() {
+	wc.Test(WordCount)
+}
